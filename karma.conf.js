4243eb01e6efc8789a2cc5edfb4dbe21b00b165c
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-10 11:43:43 +0200

    inserita configurazione browserify per risoluzione dei moduli

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: [ 'browserify', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
@@ -28,8 +28,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'spec/**/*.js': [ 'browserify' ],
+      'src/**/*.js': [ 'browserify' ]
     },
 
+    browserify: {
+      debug: true,
+      transform: [ 'brfs' ]
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
