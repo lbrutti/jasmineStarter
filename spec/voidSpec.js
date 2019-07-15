3e4fcf1d72ac2a9d04ede86958efaae1415b6cbc
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-15 15:55:02 +0200

    inserita dipendenza karma spec reporter

--- spec/voidSpec.js
+++
@@ -1,9 +0,0 @@
-describe('Selenium Grid rocks test', function() {
-    var seleniumGridRocks = null;
-    beforeEach(function() {
-      seleniumGridRocks = true;
-    });
-    it('should rock your socks off', function() {
-      expect(seleniumGridRocks).toBeTruthy();
-    })
-  });
