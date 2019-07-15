aba4d67f32645c543b1543452fa2d639532108cb
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-15 12:48:15 +0200

    spostato settaggio reporte in altro file

---
+++ e2e/settingsJourney.js
@@ -0,0 +1,17 @@
+/**
+ * just a dirty way to set jasmine reporter globally
+ */
+
+jasmine.getEnv().defaultTimeoutInterval = 10000; // in microseconds.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // in microseconds.
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+
+let consoleReporter = new JasmineConsoleReporter({
+  colors: 1,           // (0|false)|(1|true)|2
+  cleanStack: 1,       // (0|false)|(1|true)|2|3
+  verbosity: 4,        // (0|false)|1|2|(3|true)|4
+  listStyle: 'indent', // "flat"|"indent"
+  activity: true
+});
+
+jasmine.getEnv().addReporter(consoleReporter);
--- e2e/stubJourney.js
+++ e2e/stubJourney.js
@@ -1,18 +1,5 @@
 const { Builder, By, Capabilities, until, Key } = require('selenium-webdriver');
 
-jasmine.getEnv().defaultTimeoutInterval = 10000; // in microseconds.
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // in microseconds.
-const JasmineConsoleReporter = require('jasmine-console-reporter');
-
-let consoleReporter = new JasmineConsoleReporter({
-  colors: 1,           // (0|false)|(1|true)|2
-  cleanStack: 1,       // (0|false)|(1|true)|2|3
-  verbosity: 4,        // (0|false)|1|2|(3|true)|4
-  listStyle: 'indent', // "flat"|"indent"
-  activity: true
-});
-
-jasmine.getEnv().addReporter(consoleReporter);
 describe('A stub for selenium based tests', function () {
   const APP_URL = "https://www.google.com/";
   let driver;
